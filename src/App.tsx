71b0eff118f7fd613453103c70cc4197bf93f940
justnotiyan <muhammadfitrian0712@gmail.com>
2024-07-26 19:50:01 +0700

    adding lucide react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
     return (
-        <main className="flex h-screen items-center justify-center">
-            <h1 className="text-3xl font-medium text-teal-800">
-                Boilerplate by shousyadev
-            </h1>
+        <main className="app">
+            <button className="btn btn-danger"></button>
         </main>
     );
 }
