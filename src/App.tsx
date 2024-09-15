9d93ab76fa148588e0b96f9f2fa50287102cdf86
justnotiyan <muhammadfitrian0712@gmail.com>
2024-09-16 00:09:10 +0700

    Revert "first commit"
    This reverts commit 43f22b4d1bbae08e9f048af3eacd56b7f55b29ca.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Hero } from "./components/hero";
-import { Navbar } from "./components/navbar";
+import { useState } from "react";
+import cn from "./utils/cn";
 
 function App() {
     return (
-        <main>
-            <Navbar />
-            <Hero />
+        <main
+            className={cn(
+                "flex h-screen items-center justify-center bg-gray-500/10",
+            )}
+        >
+            <h1 className="text-2xl font-bold">
+                Boilterplate by{" "}
+                <a
+                    href="https://www.shousyadev.my.id/"
+                    className="text-violet-800"
+                >
+                    Shousyadev
+                </a>
+            </h1>
         </main>
     );
 }
